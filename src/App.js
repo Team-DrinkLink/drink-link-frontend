9cbc81a36e191357c9fc7501a967ebb947a53a26
Sage Jasinski <sagejasinski@gmail.com>
2022-10-10 23:45:31 +0000

    broken commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import './App.css';
 
+
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      drinks: {},
+    }
+  }
+
+componentDidMount = async ()=> {
+  console.log("hello")
+  try{
+    let PATH = `${process.env.REACT_APP_SERVER_API}s=margarita`;
+    let request = await axios.get(PATH);
+    this.setState({drinks: request.drinks})
+  }catch(error){
+    console.log("Mounting error - ", error);
+  }
+
+  // searchTerm = async (search) => {
+  //   let FIND = `${process.env.REACT_APP_SERVER_API}s={}`
+  // }
+}
+
+
   render(){
     return(
       <>
-      
+        <h1>test</h1>
       </>
     );
   }

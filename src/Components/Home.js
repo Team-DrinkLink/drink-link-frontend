7b3ae6eec96e946227073b32e4e5413438cce09b
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-10-11 02:41:08 -0500

    more modifications to footer, home.js, and sass

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,60 +1,59 @@
-
 import React from "react";
 import { Form, Container, Button, Card } from "react-bootstrap";
 import InputGroup from "react-bootstrap/InputGroup";
-import UserLoginModal from './UserLoginModal';
+import UserLoginModal from "./UserLoginModal";
 
 class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <Container className="homeContainer">
+          <Form className="form" onSubmit={this.handleSubmit}>
+            <Form.Group className="mb-3" controlId="mainSearch">
+              <InputGroup className="mb-3">
+                <Button variant="danger" type="submit">
+                  Search
+                </Button>
+                <Form.Control type="text" placeholder="Drink" variant="dark" />
+              </InputGroup>
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <div className="FilteredOptions">
+              <Form.Group controlId="searchbyingredient">
+                <Form.Check type="checkbox" label="search by ingredient" />
+              </Form.Group>
+              <Form.Group controlId="non-alcoholic">
+                <Form.Check type="checkbox" label="non-alcoholic" />
+              </Form.Group>
+            </div>
+          </Form>
+        </Container>
 
-    render() {
-        return (
-            <>
-              <Container className="homeContainer">
-                <Form className="form" onSubmit={this.handleSubmit}>
-                  <Form.Group className="mb-3" controlId="mainSearch">
-                    <InputGroup className="mb-3">
-                      <Button variant="primary" type="submit">
-                              Search
-                      </Button>
-                      <Form.Control type="text" placeholder="Drink" />
-                    </InputGroup>
-                          <Form.Text className="text-muted"></Form.Text>
-                      </Form.Group>
-                        <div className="FilteredOptions">
-                          <Form.Group controlId="searchbyingredient">
-                            <Form.Check type="checkbox" label="search by ingredient" />
-                          </Form.Group>
-                          <Form.Group controlId="non-alcoholic">
-                            <Form.Check type="checkbox" label="non-alcoholic" />
-                          </Form.Group>
-                        </div>
-                      </Form>
-              </Container>
+        {this.props.drinkResults.length && (
+          <Container className="cardDisplay">
+            {this.props.drinkResults.map((drink, index) => {
+              let newCard = (
+                <Card style={{ width: "18rem" }} key={index}>
+                  {" "}
+                  <Card.Header>{drink.strDrink}</Card.Header>
+                  <Card.Body>
+                    <Card.Img src={drink.strDrinkThumb} />
+                  </Card.Body>
+                </Card>
+              );
 
-                  {this.props.drinkResults.length &&
-                <Container className='cardDisplay'>
-
-                    {this.props.drinkResults.map((drink,index) =>{
-                        
-                        let newCard = <Card style={{ width: '18rem' }} key={index}> <Card.Header>{drink.strDrink}</Card.Header><Card.Body><Card.Img src={drink.strDrinkThumb}/></Card.Body></Card>
-
-                        return newCard
-                    
-                        })
-                    }
-                </Container>
-
-                    }
-                <UserLoginModal
-                    showModal={this.props.showModal}
-                    hideModal={this.props.hideModal}
-                    handleUserCreate={this.handleUserCreate}
-                    />
-
-            </>
-        )
-
-    }
+              return newCard;
+            })}
+          </Container>
+        )}
+        <UserLoginModal
+          showModal={this.props.showModal}
+          hideModal={this.props.hideModal}
+          handleUserCreate={this.handleUserCreate}
+        />
+      </>
+    );
+  }
 }
 
 export default Home;

fdc33be9408e37a306bf1c1acbdfc4ea8ca0920e
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-10-12 18:09:29 -0500

    added functionality to favorites page and created logo

--- src/App.js
+++ src/App.js
@@ -16,97 +16,107 @@ class App extends React.Component {
       selectedDrink: {},
       userFavorites: [],
       userLoggedIn: {},
-      searchTerm: '',
-      showModal: false,   
+      searchTerm: "",
+      showModal: false,
+    };
+  }
 
+  componentDidMount() {
+    this.getDrinks();
+    // console.log(this.state.drinkResults)
+  }
+
+  submitListener = (event) => {
+    event.preventDefault();
+    let search =
+      this.state.searchTerm === ""
+        ? ""
+        : this.searchDrink(this.state.searchTerm);
+    return search;
+  };
+
+  searchTermChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  getDrinks = async () => {
+    //Sage: sets initial images to be margaritas until search changes the results
+    try {
+      let PATH = `${process.env.REACT_APP_SERVER_API}s=margarita`;
+      // let PATH = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`
+      let request = await axios.get(PATH);
+      // console.log(request.data);
+      this.setState({ drinkResults: request.data.drinks });
+    } catch (error) {
+      console.log("Mounting error - ", error);
     }
-  }
+  };
 
-componentDidMount() {
-  this.getDrinks()
-  // console.log(this.state.drinkResults)
-}
+  searchDrink = async (term) => {
+    try {
+      let GRAB = `${process.env.REACT_APP_SERVER_API}s=${term}`;
+      let request = await axios.get(GRAB);
+      console.log(request.data.drinks);
+      this.setState({ drinkResults: request.data.drinks });
+    } catch (error) {
+      console.log("searching error - ", error);
+    }
+  };
 
-submitListener = (event)=> {
-  event.preventDefault();
-   let search = this.state.searchTerm === '' ? '': this.searchDrink(this.state.searchTerm);
-  return search;
-}
-
-searchTermChange = (event) => {
-  this.setState({searchTerm: event.target.value})
-}
-
-getDrinks = async () => {
-  //Sage: sets initial images to be margaritas until search changes the results
-  try{
-    let PATH = `${process.env.REACT_APP_SERVER_API}s=margarita`;
-    // let PATH = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`
-    let request = await axios.get(PATH);
-    // console.log(request.data);
-    this.setState({drinkResults: request.data.drinks}); 
-  }catch(error){
-    console.log("Mounting error - ", error);
-  }
-}
- searchDrink = async (term) => {
-  try {
-    let GRAB = `${process.env.REACT_APP_SERVER_API}s=${term}`
-    let request = await axios.get(GRAB);
-    console.log(request.data.drinks)
-    this.setState({drinkResults: request.data.drinks})
-  } catch (error) {
-    console.log("searching error - ", error)
-  }
- }
-
-//User create functions//////////
-handleUserCreate = async (newUserInfo) => {
-  console.log ('User Created is:', newUserInfo);
-  try {
-      const res = await axios.post(`${process.env.REACT_APP_SERVER}`, newUserInfo);
+  //User create functions//////////
+  handleUserCreate = async (newUserInfo) => {
+    console.log("User Created is:", newUserInfo);
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_SERVER}`,
+        newUserInfo
+      );
       const createdUser = res.data;
       console.log(res.data);
       this.setState({
-          userLoggedIn: createdUser,
-      })
-  } catch (error) {
+        userLoggedIn: createdUser,
+      });
+    } catch (error) {
       console.log("Error, there is a problem: ", error.response);
-  }
-}
+    }
+  };
 
-loginClick = (e) => {
-  e.preventDefault();
-  this.setState({showModal: true});
-}
+  loginClick = (e) => {
+    e.preventDefault();
+    this.setState({ showModal: true });
+  };
 
-hideModal =() => {
-  this.setState({showModal: false});
-}
+  hideModal = () => {
+    this.setState({ showModal: false });
+  };
 
   render() {
     return (
       <>
-      <Router>
-        <Header 
-        loginClick={this.loginClick}
-        hideModal={this.hideModal}/>
-        <Routes>
-          <Route
-            exact path="/"
-            element = {<Home
-            drinkResults={this.state.drinkResults}
-            handleUserCreate={this.handleUserCreate}
-            showModal={this.state.showModal}
-            search={this.searchTermChange}
-            submit ={this.submitListener}
-            />}
-            >
-            </Route>
+        <Router>
+          <Header loginClick={this.loginClick} hideModal={this.hideModal} />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Home
+                  drinkResults={this.state.drinkResults}
+                  handleUserCreate={this.handleUserCreate}
+                  showModal={this.state.showModal}
+                  search={this.searchTermChange}
+                  submit={this.submitListener}
+                />
+              }
+            ></Route>
 
             <Route exact path="/drink" element={<Drink />}></Route>
 
-            <Route exact path="/favorites" element={<Favorites />}></Route>
+            <Route
+              exact
+              path="/favorites"
+              element={<Favorites userFavorites={this.state.userFavorites} />}
+            ></Route>
           </Routes>
           <Footer />
         </Router>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,26 +4,32 @@ import { Navbar, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import AuthButtons from "./AuthButtons";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../assets/black.png";
 
 class Header extends React.Component {
   render() {
     return (
-      
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Welcome to DrinkLink</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height={38} width={200} />
+        </Navbar.Brand>
 
         <div className="NavItems">
-        <NavItem>
-          <Link to="/" className="nav-link" style={{ color: "white" }}>
-            Home
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/favorites" className="nav-link" style={{ color: "white" }}>
-            Favorites
-          </Link>
-        </NavItem>
-        <AuthButtons />
+          <NavItem>
+            <Link to="/" className="nav-link" style={{ color: "white" }}>
+              Home
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link
+              to="/favorites"
+              className="nav-link"
+              style={{ color: "white" }}
+            >
+              Favorites
+            </Link>
+          </NavItem>
+          <AuthButtons />
         </div>
       </Navbar>
     );
